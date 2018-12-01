122e91efc3f47d9f3e9e15d41b68f01be1c45ac6
Dung Truong <checkraiser11@gmail.com>
2018-12-01 09:34:30 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const { parse } = require('url')
+const { join } = require('path')
+const pathMatch = require('path-match')
+const route = pathMatch()
+const pathToRegexp = require('path-to-regexp')
+const url = require('url')
+
+function routesMiddleware({server, handle, app, dev}, {defaultRoutes, nowRoutes}) {
+  let additionalRoutes = {}
+  nowRoutes.routes.forEach(function(item) {
+    additionalRoutes[item.src] = function({app, req, res, query}) {
+      var re = pathToRegexp(item.src)
+      const result = re.exec(req.url)
+      if (result) {
+        const slices = result.slice(1, result.length)
+        let tmp = item.dest
+        for(let i = 0; i < slices.length; i++) {
+          tmp = tmp.replace(`$${i+1}`, slices[i])
+        }
+        const t = url.parse(tmp, true)
+        const finalQuery = {...t.query, ...query}
+        app.render(req, res, t.pathname, finalQuery)
+      }
+    }
+  })
+  const routes = defaultRoutes(additionalRoutes)
+  const MobileDetect = require('mobile-detect')
+  server.get('*', (req, res, next) => {
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+    
+    const md = new MobileDetect(req.headers['user-agent']);
+    const isMobile = md.mobile()
+    for (let k in routes) {
+      if (routes.hasOwnProperty(k)) {
+        const params = route(k)(pathname)
+        if (params) {
+          return routes[k]({app, req, res, next, handle, query, pathname, isMobile, join, params, dev})
+        }
+      }
+    }
+  });
+}
+
+module.exports = routesMiddleware
